2ce238450bbfdc761565410a461cb3a2047d313f
Daniel Carvallo <elulcao@icloud.com>
2025-06-13 18:01:22 -0600

    fix: fall back to original gethostname when NEWHOSTNAME unset

--- gethostname.c
+++ gethostname.c
@@ -1,17 +1,30 @@
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <dlfcn.h>
 
 // gethostname() - get host name of machine running this program
 int gethostname(char *name, size_t len)
 {
+  static int (*orig_gethostname)(char *, size_t) = NULL;
+
+  if (!orig_gethostname)
+  {
+    orig_gethostname = dlsym(RTLD_NEXT, "gethostname");
+  }
+
   if (len < 10)
   {             // check if the buffer is large enough
     return (1); // return 1 if len is less than 10
   }
+
   if (getenv("NEWHOSTNAME")) // check if the environment variable NEWHOSTNAME is set
   {
     strncpy(name, getenv("NEWHOSTNAME"), len); // copy the value of the environment variable NEWHOSTNAME to the buffer
+  }
+  else if (orig_gethostname) // if env var not set, call the original gethostname
+  {
+    return orig_gethostname(name, len);
   }
 
   return 0; // return 0 if everything is ok
